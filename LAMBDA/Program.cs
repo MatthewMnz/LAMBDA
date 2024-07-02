2d374810ad37bfa0fdb9b740c8f9095034ca8f00
MatthewMnz <mattalex28@gmail.com>
2024-07-01 18:11:55 -0600

    Funcion Independiente Agregada

--- LAMBDA/Program.cs
+++ LAMBDA/Program.cs
@@ -1,30 +1,34 @@
 ﻿namespace LAMBDA
 {
-    internal class Program
-    {
-        static void Main(string[] args)
+    class Program
+    { 
+        static void Main (String[] args)
         {
-            Console.WriteLine("LAMBDA Progra I");
-
-            Func<int, int, int> suma = (a, b) => a + b;
-            Console.WriteLine(suma(3, 4));
-
-            Func<double, double> prod = x => x * x; 
-            Console.WriteLine(prod(10));
-
-            Func<int, string, bool> isTooLong = (int x, string s) => s.Length > x;
-            Console.WriteLine(isTooLong(10, "Progra I"));
-
-            Action<int> MayorEdad = n =>
+            List<Student> estudiantes = new List<Student>
             {
-                Boolean ME = (n > 18) ? true : false;
-                Console.WriteLine($"Es mayor de edad {ME} ");
+                new Student("Ismael Asensio", 20, 90),
+                new Student("Juan Espinoza", 19, 70),
+                new Student("Nestor Soto", 20, 80),
+                new Student("Keneth Palacios", 19, 80),
+                new Student("Alvaro Garay", 20, 60)
             };
 
-            MayorEdad(15);
+            var estudianteNotaAlta = estudiantes.OrderByDescending(e => e.Nota).FirstOrDefault();
+            Console.WriteLine($"El estudiante con la nota mas alta es: {estudianteNotaAlta.Nombre} - Nota: {estudianteNotaAlta.Nota}");
 
-            Action<string> saludar = _ => Console.WriteLine("Hola");
-            saludar("Ignorado"); 
+            var estudiantesPorOrden = estudiantes.OrderBy(e => e.Nombre).ToList();
+            Console.WriteLine("Lista de estudiantes ordenados por su nombre:");
+            foreach (var student in estudiantesPorOrden)
+            {
+                Console.WriteLine($"{student.Nombre} - Edad: {student.Edad} - Nota: {student.Nota}");
+            }
+
+            var promedioNotas = estudiantes.Average(e => e.Nota);
+            Console.WriteLine($"Promedio de notas de estudiantes: {promedioNotas}");
+
+            Console.ReadLine();
         }
     }
+
+
 }
